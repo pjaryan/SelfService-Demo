a1f7f791653bf3271f0f1c3126ae6ca491a63048
Andrew Way <away@env0.com>
2022-11-30 15:44:07 -0800

    tff

--- simple-terraform/main.tf
+++ simple-terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.1.0"
     }
   }
   backend "remote" {
-    hostname = "backend.api.env0.com"
+    hostname     = "backend.api.env0.com"
     organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.6de20c9c-e5a2-49a3-aee5-00c03204ec3a"
 
     workspaces {
@@ -16,19 +16,19 @@ terraform {
 }
 
 variable "refresh_date" {
-  type = string
+  type    = string
   default = "00/00/0000"
 }
 
 resource "random_string" "random" {
   keepers = {
-      refresh_date = var.refresh_date
+    refresh_date = var.refresh_date
   }
-  length           = 5
-  upper            = true
-  special          = false
+  length  = 5
+  upper   = true
+  special = false
 }
 
 output "random_string" {
-    value = random_string.random.result
+  value = random_string.random.result
 }
